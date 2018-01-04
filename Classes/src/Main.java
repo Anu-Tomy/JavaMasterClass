f3c18e724400d7bdf9d8ef7cca7e588c5a49b217
Anu Tomy <anuatomy@gmail.com>
2018-01-04 08:45:42 -0500

    Example of classes

---
+++ Classes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car Toyota = new Car();
+        Car Honda = new Car();
+
+        Toyota.setModel("toyota");
+        System.out.println("Model is " + Toyota.getModel());
+    }
+}
