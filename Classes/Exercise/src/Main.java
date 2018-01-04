d8cf4aae99e10ffd3eee876e44c3c9d6b01abac3
Anu Tomy <anuatomy@gmail.com>
2018-01-04 17:54:57 -0500

    Complete the exercise for bank account

--- Classes/Exercise/src/Main.java
+++ Classes/Exercise/src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         // Create a new class for a bank account
         // Create fields for the account number, balance, customer name, email and phone number.
-        //
         // Create getters and setters for each field
         // Create two additional methods
         // 1. To allow the customer to deposit funds (this should increment the balance field).
