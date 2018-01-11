e249d97421e130f36c78daf1fb464bd6847f3864
Anu Tomy <anuatomy@gmail.com>
2018-01-11 12:00:57 -0500

    Example of Array using methods

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -3,12 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         int[] myIntArray = new int[10];
-        for(int i = 0; i < myIntArray.length; i++){
-         myIntArray[i] = i;
-        }
-        for(int i = 0; i < myIntArray.length; i++){
-            System.out.println("Element " + i + "value is " + myIntArray[i]);
-        }
 
+        for(int i = 0; i < myIntArray.length; i++){
+            myIntArray[i] = i;
+        }
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Element " + i + " value is " + array[i]);
+        }
     }
 }
