4ef9464cac7d464e20ad7a4166b7179069167883
Anu Tomy <anuatomy@gmail.com>
2018-01-11 11:54:38 -0500

    Example of Arrays

---
+++ Arrays/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[10];
+        for(int i = 0; i < myIntArray.length; i++){
+         myIntArray[i] = i;
+        }
+        for(int i = 0; i < myIntArray.length; i++){
+            System.out.println("Element " + i + "value is " + myIntArray[i]);
+        }
+
+    }
+}
