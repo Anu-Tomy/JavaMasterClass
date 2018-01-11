016b5a2e951c5bfe0e30eb97b208ea72cffab934
Anu Tomy <anuatomy@gmail.com>
2018-01-11 12:33:34 -0500

    Example of using Scanner

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,18 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner((System.in));
+
     public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
 
-        int[] myIntArray = new int[10];
-
-        for(int i = 0; i < myIntArray.length; i++){
-            myIntArray[i] = i;
+        for(int i = 0; i < myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was "+ myIntegers[i]);
         }
-        printArray(myIntArray);
+        System.out.println("The Average is " + getAverage(myIntegers));
+
     }
 
-    public static void printArray(int[] array){
-        for(int i = 0; i < array.length; i++){
-            System.out.println("Element " + i + " value is " + array[i]);
+
+    public static  int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i =0 ; i < values.length; i++){
+            values[i] = scanner.nextInt();
         }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for(int i = 0 ; i < array.length; i++){
+            sum += array[i];
+
+        }
+        return ((double) sum / array.length);
+
     }
 }
