ae87c2052e0a1848be28ac8e4fc06e0f72de9c85
Anu Tomy <anuatomy@gmail.com>
2018-01-18 11:20:43 -0500

    Fix errors

--- OOP-MasterChallengeExercise/src/DeluxeHamburger.java
+++ OOP-MasterChallengeExercise/src/DeluxeHamburger.java
@@ -1,6 +1,6 @@
 public class DeluxeHamburger extends Hamburger {
     public DeluxeHamburger() {
-        super("Deluxe", "Beef", 14.99, "White");
+        super("Deluxe", "bacon and suasage", 14.99, "White");
         super.addHamburgerAddition1("Chips", 2.75);
         super.addHamburgerAddition2("Drink", 1.25);
     }
