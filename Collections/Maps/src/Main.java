b6542e7804378d95d5f225a9383aff7049b09cb7
Anu Tomy <anuatomy@gmail.com>
2018-02-21 11:32:27 -0500

    Examples of map

--- Collections/Maps/src/Main.java
+++ Collections/Maps/src/Main.java
@@ -7,13 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
+
+        languages.put("Java","a compiled high level object-oriented, platform independent language" );
+        languages.put("Python", "an interpreted,object-oriented, high-level programming language with dynamic semantic");
+        languages.put("Algol", "an algorithmic language");
+
+        for(String key: languages.keySet()){
+            System.out.println(key + " : " + languages.get(key));
+        }
+        System.out.println("=========================================");
+
         if(languages.containsKey("Java")){
-            System.out.println("Java already exists");
+            System.out.println("Java already exists in the map");
         }else{
             languages.put("Java", "a compiled high level object-oriented, platform independent language");
         }
-        System.out.println(languages.get("Java"));
+        //System.out.println(languages.get("Java"));
     }
-
 
 }
